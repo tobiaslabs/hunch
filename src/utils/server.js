dd20cda9b240a072e18685bb5a1dcd75d0741e1d
Tobias Davis <tobias@davistobias.com>
2023-02-20 16:48:08 -0600

    feature: prettify local dev server logs

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,24 +1,35 @@
+import { createServer } from 'node:http'
+import { setTimeout as timeoutDelay } from 'node:timers'
+
 import { fromQuery } from '../from-query.js'
 import { hunch } from '../hunch.js'
 
-import { createServer } from 'node:http'
+const prettify = url => {
+	let [ pre, query ] = url.split('?')
+	const parts = []
+	for (const chunk of query.split('&')) {
+		const [ key, value ] = chunk.split('=')
+		parts.push(`${decodeURIComponent(key)}=${decodeURIComponent(value)}`)
+	}
+	query = parts.sort().join('&')
+	return pre + '?' + query
+}
 
 const HOSTNAME = '127.0.0.1'
 let server
-export const startServer = ({ port, index }) => {
+export const startServer = ({ port, index, delay }) => {
 	if (server) server.close(() => {
 		console.log('HunchJS server restarting due to index changes...')
 		server = undefined
-		setTimeout(() => { startServer({ port, index }) })
+		setTimeout(() => { startServer({ port, index, delay }) })
 	})
 	else {
 		const search = hunch({ index })
 		server = createServer((req, res) => {
-			console.log(new Date(), req.method, req.url)
+			console.log(new Date(), req.method, prettify(req.url))
 			const { searchParams } = new URL(`https://${HOSTNAME}${req.url}`)
 			let body
 			let statusCode = 200
-			res.setHeader('content-type', 'application/json')
 			try {
 				const query = fromQuery(searchParams)
 				body = search(query)
@@ -31,8 +42,11 @@ export const startServer = ({ port, index }) => {
 					},
 				}
 			}
-			res.statusCode = statusCode
-			res.end(JSON.stringify(body, undefined, 4))
+			timeoutDelay(() => {
+				res.setHeader('content-type', 'application/json')
+				res.statusCode = statusCode
+				res.end(JSON.stringify(body, undefined, 4))
+			}, delay)
 		})
 		server.listen(port, () => {
 			console.log(`HunchJS server running: http://${HOSTNAME}:${port}/`)
