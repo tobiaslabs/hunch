4c0012d9036aa69febd6fd7e8b3174890275a693
Tobias Davis <tobias@davistobias.com>
2023-06-14 20:38:28 -0500

    test: reformat for readability

--- test/feature/huge-text-search/basic.test.js
+++ test/feature/huge-text-search/basic.test.js
@@ -13,10 +13,18 @@ export default ({ assert, hunch, index }) => [
 					{
 						_id: 'romans/chapter-6.md',
 						_score: 6.103,
-						_chunks: [ { name: 'markdown', content: 'u should obey it in its lusts.  Also, do not present your members to sin as instruments of unrighteo' } ],
+						_chunks: [
+							{
+								name: 'markdown',
+								content: 'u should obey it in its lusts.  Also, do not present your members to sin as instruments of unrighteo',
+								snippet: {
+									content: { start: 1264, size: 2644 },
+								},
+							},
+						],
 					},
 				],
-				page: { offset: 0, size: 1, pages: 69, items: 69 },
+				page: { offset: 0, size: 1, pages: 69, items: 2 },
 			},
 			'make sure basic text lookup works',
 		)
@@ -38,7 +46,15 @@ export default ({ assert, hunch, index }) => [
 					{
 						_id: 'romans/chapter-6.md',
 						_score: 98.146,
-						_chunks: [ { name: 'markdown', content: 'hould obey it in its lusts.  Also, do not present your members to sin as instruments of unrighteousn' } ],
+						_chunks: [
+							{
+								name: 'markdown',
+								content: 'hould obey it in its lusts.  Also, do not present your members to sin as instruments of unrighteousn',
+								snippet: {
+									content: { start: 1267, size: 2644 },
+								},
+							},
+						],
 					},
 				],
 				page: { offset: 0, size: 1, pages: 775, items: 775 },
